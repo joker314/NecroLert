01b85896bf4586383c3dbbb4390437a08b951cb8
Max <maxzuba22@yahoo.co.uk>
2016-07-08 16:07:44 +0100

    Create necro.user.js

---
+++ necro.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name NecroLert
+// @namespace http://joker314.github.io
+// @version 1.1
+// @description  Shows a visual warning if you are maing a necropost on Scratch
+// @match *://scratch.mit.edu/discuss/topic/*
+// ==/UserScript==
