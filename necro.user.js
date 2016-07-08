b1f8c4fb6eadfde1e1e6cb8641b9bac35f8e609d
Max <maxzuba22@yahoo.co.uk>
2016-07-08 17:09:42 +0100

    Update necro.user.js

--- necro.user.js
+++ necro.user.js
@@ -5,3 +5,22 @@
 // @description  Shows a visual warning if you are maing a necropost on Scratch
 // @match *://scratch.mit.edu/discuss/topic/*
 // ==/UserScript==
+function getDate(str){
+  var d = str;
+  d = d.replace("Today", new Date().getFullYear() + "-" + new Date().getMonth() + "-" + new Date().getDate();
+  d = d.replace("Yesterday", new Date(Date.now() - 86400000).getFullYear() +
+    "-" + new Date(Date.now() - 86400000).getMonth() + 
+    "-" + new Date(Date.now() - 86400000).getDate());
+  return new Date(d);
+}
+var postheaders = document.getElementsByClassName("box-head");
+
+for (var i = 1; i < postheaders.length; i++){
+ if (getDate(postheaders[i].children[1]).valueOf() - getDate(postheaders[i].children[1]).valueOf() > 1814400000){
+   postheaders[i].style.backgroundColor = "red";
+ }
+}
+
+if(getDate(postheaders[postheaders.length].children[1]).valueOf() - new Date().valueOf() > 1814400000){
+  document.getElementsByName("AddPostForm")[0].style.backgroundColor = "red";
+}
