0c358e83537496caa02d05ce6a6b2361fe95a3d0
Max <maxzuba22@yahoo.co.uk>
2016-07-08 23:39:13 +0100

    Update necro.user.js

--- necro.user.js
+++ necro.user.js
@@ -8,21 +8,21 @@
 function getDate(el){
   console.log(el);
   var d = el.innerHTML;
-  var d = d.replace("Today", new Date().getFullYear() + "-" + new Date().getMonth() + "-" + new Date().getDate());
+  var d = d.replace("Today", new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate());
   var d = d.replace("Yesterday", new Date(Date.now() - 86400000).getFullYear() +
-    "-" + new Date(Date.now() - 86400000).getMonth() + 
+    "-" + (new Date(Date.now() - 86400000).getMonth() + 1) + 
     "-" + new Date(Date.now() - 86400000).getDate());
   return new Date(d);
 }
 var postheaders = document.querySelectorAll(".blockpost .box-head");
 
 for (var i = 1; i < postheaders.length; i++){
- if (getDate(postheaders[i].children[1]).valueOf() - getDate(postheaders[i - 1].children[1]).valueOf() > 1814400000){
+ if (getDate(postheaders[i].children[1]).valueOf() - getDate(postheaders[i - 1].children[1]).valueOf() > 5097600000){
    postheaders[i].style.backgroundColor = "red";
  }
 }
 
-if(new Date().valueOf() - getDate(postheaders[postheaders.length - 1].children[1]).valueOf()   > 1814400000){
+if(new Date().valueOf() - getDate(postheaders[postheaders.length - 1].children[1]).valueOf()   > 5097600000){
   document.getElementsByName("AddPostForm")[0].className = "button";
   document.getElementsByName("AddPostForm")[0].style.color = "red";
   document.getElementsByName("AddPostForm")[0].children[0].innerHTML = "Necropost this";
